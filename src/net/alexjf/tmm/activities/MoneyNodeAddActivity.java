e483bb4f1566d9f2169b161b69039ee93b219029
alexjf <alexandrejorgefonseca@gmail.com>
2013-01-29 00:12:25 +0100

    Added small copyright notices to all files
    Also removed eclipse-generated icon

--- src/net/alexjf/tmm/activities/MoneyNodeAddActivity.java
+++ src/net/alexjf/tmm/activities/MoneyNodeAddActivity.java
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * Copyright (c) 2013 - Alexandre Jorge Fonseca (alexjf.net)
+ * License: GPL v3 (http://www.gnu.org/licenses/gpl-3.0.txt)
+ ******************************************************************************/
 package net.alexjf.tmm.activities;
 
 import java.math.BigDecimal;
